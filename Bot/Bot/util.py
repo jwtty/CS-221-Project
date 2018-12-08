b239e6543fb3434109bc35975a098150f09db03f
姜宛彤 <jiangwantong@DN51skv3.SUNet>
2018-12-08 14:30:51 -0800

    support arguments

---
+++ Bot/Bot/util.py
@@ -0,0 +1,12 @@
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--test_rl", type=str, default=False, help="whether to test a rl agent (sets explore prob to 0)")
+    parser.add_argument("--rl_weights", type=str, default=None, help="the weight file for RL agent, used for restore weight parameters")
+    parser.add_argument("--rl_save", type=str, default=None, help="file name to save the weigts for RL agent")
+    parser.add_argument("--rl_trial", type=int, default=2000, help="num_trials the rl is going to use")
+    parser.add_argument("--mode", type=str, default="rl", help="game|rl whether to enter game play or rl training")
+    parser.add_argument("--grid", type=int, default=4, help="size of grid, should match with grid used for rl training if hoping to use")
+    args = parser.parse_args()
+    return args
--- Bot/main.py
+++ Bot/main.py
@@ -4,16 +4,28 @@ import sys
 from Bot.simulator import Game
 from Bot.bot import Bot
 from Bot.rl import RLAlgorithm
+from Bot.util import parse_args
 
-def main():
+def main(grid_size):
     bot = Bot()
     bot2 = Bot()
     game = Game()
+    game.field_width = game.field_height = grid_size
     game.run(bot, bot2)
 
-def mainrl():
+def mainrl(trials, save, read, test, grid):
     rl = RLAlgorithm(0.99)
-    rl.train(verbose=True)
+    rl.train(verbose=True, num_trials=trials, save_file=save, read_file=read, test = test, grid_size=grid)
+    
 if __name__ == '__main__':
-    #main()
-    mainrl()
+    args = parse_args()
+    mode = args.mode
+    grid_size = args.grid
+    if mode == "game":
+        main(grid_size)
+    elif mode == "rl":
+        read_file = args.rl_weights
+        save_file = args.rl_save
+        test = args.test_rl
+        trials = args.rl_trial
+        mainrl(trials, save_file, read_file, test, grid_size)
